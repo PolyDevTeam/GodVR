52b2ebab6f0e23c5ba9aeae06c79b30ec7df3614
Stamegna <clement.stamegna@hotmail.fr>
2019-01-23 18:00:21 +0100

    add fireball spell, test fireball on pnj, add _remove dead pnj_ script

---
+++ Assets/Components/GodPower/PyroParticles/HavePV.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HavePV : MonoBehaviour
+{
+
+    public int pv = 3;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.gameObject.name);
+
+        if (collisionInfo.gameObject.name.Contains("Fire"))
+        {
+            pv--;
+            Debug.Log(pv);
+            if (pv == 0)
+            {
+                this.GetComponent<Civil>().setLife(0);
+                //Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Components/GodPower/PyroParticles/shootFireball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootFireball : MonoBehaviour
+{
+
+    public GameObject Fireball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("FIRE");
+
+            Instantiate(Fireball, this.transform);
+        }
+    }
+}
